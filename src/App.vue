2f0cfe3bde5936ebb9f01c4f99e22a6943378e27
yz-gm <65427441+yz-gm@users.noreply.github.com>
2023-06-08 13:23:05 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <!--  <img alt="Vue logo" src="./assets/logo.png">-->
+  <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <OttoHzys></OttoHzys>
+</template>
+
+<script>
+import OttoHzys from "@/components/OttoHzys"
+
+export default {
+  name: 'App',
+  components: {
+    OttoHzys
+  }
+}
+</script>
+
+<style>
+</style>
